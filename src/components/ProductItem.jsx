cecfc24fc7b3493d604710a8275deb2ae32bdf88
ELITGX <elitgx@gmail.com>
2022-12-13 01:58:29 -0600

    Se agregan cambios hasta la clase 20 del curso

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+	const [cart, setCart] = useState([]);
+
+	const handleClick = () => {
+		setCart([]);
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,7 +17,7 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick} >
 					<img src={addToCart} alt="addToCart" />
 				</figure>
 			</div>
