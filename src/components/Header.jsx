1a3dce5c1597bc7f2250f45999aab8f90d115259
ELITGX <elitgx@gmail.com>
2022-12-15 00:15:18 -0600

    Se agregan cambios hasta la clase 26 donde utilizamos custom hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
+import MyOrder from '../containers/MyOrder'
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
+import AppContext from '../context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
+	const { state } = useContext(AppContext);
 
 	const handleToggle = () => {
 		setToggle(!toggle);
@@ -43,13 +47,15 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						platzi@example.com
 						</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-shopping-cart"
+					onClick={() => setToggleOrders(!toggleOrders)}>
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
 					</li>
 				</ul>
 			</div>
 			{toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 		</nav>
 	);
 }
