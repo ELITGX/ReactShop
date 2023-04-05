2dd0374eca483ff7ff1a811125f930a44a4ed656
Gustavo Ortega <75866689+ELITGX@users.noreply.github.com>
2023-04-05 11:53:09 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ const Header = () => {
 						className="navbar-email"
 						onClick={handleClickDesktop}
 					>
-						matiaschiodo
+						Gustavo Ortega
 					</li>
 					<li
 						className="navbar-shopping-cart"
